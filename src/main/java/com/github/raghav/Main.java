34aa6d5f00b0a8000d779009a8e39b4a44bc4f6f
Raghav Aggarwal <raghavaggarwal03.ra@gmail.com>
2025-07-05 11:53:35 +0530

    spotless check

--- src/main/java/com/github/raghav/Main.java
+++ src/main/java/com/github/raghav/Main.java
@@ -1,41 +1,48 @@
+/*
+Copyright 2025 Raghav Aggarwal
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.github.raghav;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
-import java.sql.SQLException;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        try {
-            String driverName = "org.apache.hive.jdbc.HiveDriver";
-            Class.forName(driverName);
-        }
-        catch(ClassNotFoundException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        Connection con = DriverManager.getConnection("jdbc:hive2://localhost:10000/acid", "", "");
-        Statement stmt = con.createStatement();
-        String tableName = "tbl1";
-        stmt.execute("create table " + tableName + " (id int) STORED AS ORC TBLPROPERTIES" +
-                     "('transactional'='true')");
-
-        String sql = "describe formatted " + tableName;
-        System.out.println("Running: " + sql);
-        ResultSet res = stmt.executeQuery(sql);
-
-        while(res.next())
-            System.out.println(res.getString(1) + "\t" + res.getString(2));
-
-        stmt.close();
-        con.close();
+  public static void main(String[] args) throws SQLException {
+    try {
+      String driverName = "org.apache.hive.jdbc.HiveDriver";
+      Class.forName(driverName);
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+      System.exit(1);
     }
 
-}
+    Connection con = DriverManager.getConnection("jdbc:hive2://localhost:10000/raghav", "", "");
+    Statement stmt = con.createStatement();
+    String tableName = "t2";
+    stmt.execute("select `(COUNT)?.` from t2 where id=1");
 
+    String sql = "describe formatted " + tableName;
+    System.out.println("Running: " + sql);
+    ResultSet res = stmt.executeQuery(sql);
+
+    while (res.next()) System.out.println(res.getString(1) + "\t" + res.getString(2));
+
+    stmt.close();
+    con.close();
+  }
+}
